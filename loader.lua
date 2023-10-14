5f8c4b29686a95b0b805ed601fca4a561c2f45d4
OopssSorry <96795888+OopssSorry@users.noreply.github.com>
2023-10-14 14:02:32 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local old
+old = hookfunction(Game.HttpGet, function(self, link)
+    if (link):find("raw.githubusercontent.com/boot-could") then
+      return old(self, (link):gsub("boot-could/luax","OopssSorry/luax-crack"))
+    elseif (link):find("sirius.menu/rayfield") then
+      return old(self, "https://raw.githubusercontent.com/OopssSorry/luax-crack/main/raifield_whiteout_ks.lua")
+    end
+    return old(self, link)
+end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/boot-could/luax/main/luaxtrialxd.lua"))()
